67b6ff7b4dce775bf77e93c44bb8a6e4e86a19c6
smh <dbd05088@naver.com>
2023-01-10 23:33:58 +0900

    first commit

---
+++ baseline.sh
@@ -0,0 +1,52 @@
+#/bin/bash
+
+# CIL CONFIG
+NOTE="baseline_sample_mem50000_lamb_1" # Short description of the experiment. (WARNING: logs/results with the same note will be overwritten!)
+MODE="baseline"
+DATASET="cifar10" # cifar10, cifar100, tinyimagenet, imagenet
+SIGMA=10
+REPEAT=1
+INIT_CLS=100
+GPU_TRANSFORM="--gpu_transform"
+HUMAN_TRAINING="True"
+USE_AMP="--use_amp"
+SEEDS="1 2 3"
+RECENT_RATIO="0.8"
+T="8"
+
+if [ "$DATASET" == "cifar10" ]; then
+    MEM_SIZE=50000 ONLINE_ITER=1
+    MODEL_NAME="resnet18" EVAL_PERIOD=100
+    BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+
+elif [ "$DATASET" == "cifar100" ]; then
+    MEM_SIZE=2000 ONLINE_ITER=3
+    MODEL_NAME="resnet18" EVAL_PERIOD=100
+    BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+
+elif [ "$DATASET" == "tinyimagenet" ]; then
+    MEM_SIZE=4000 ONLINE_ITER=3
+    MODEL_NAME="resnet18" EVAL_PERIOD=100
+    BATCHSIZE=32; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+
+elif [ "$DATASET" == "imagenet" ]; then
+    MEM_SIZE=20000 ONLINE_ITER=0.25
+    MODEL_NAME="resnet18" EVAL_PERIOD=1000
+    BATCHSIZE=256; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=10
+
+else
+    echo "Undefined setting"
+    exit 1
+fi
+
+for RND_SEED in $SEEDS
+do
+    CUDA_VISIBLE_DEVICES=1 nohup python main.py --mode $MODE \
+    --dataset $DATASET --T $T \
+    --sigma $SIGMA --repeat $REPEAT --init_cls $INIT_CLS\
+    --rnd_seed $RND_SEED --use_human_training $HUMAN_TRAINING \
+    --model_name $MODEL_NAME --opt_name $OPT_NAME --sched_name $SCHED_NAME \
+    --lr $LR --batchsize $BATCHSIZE --recent_ratio $RECENT_RATIO \
+    --memory_size $MEM_SIZE $GPU_TRANSFORM --online_iter $ONLINE_ITER \
+    --note $NOTE --eval_period $EVAL_PERIOD --imp_update_period $IMP_UPDATE_PERIOD $USE_AMP &
+done
